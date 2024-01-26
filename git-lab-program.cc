4740a6ba73ce228e9940e9432f4960441ccec34f
Tristan <tp935121@ohio.edu>
2024-01-26 15:40:25 -0500

    hi

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Tristan Prater
+ *        Date: 01/26/24
+ * Description: Git lab program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
